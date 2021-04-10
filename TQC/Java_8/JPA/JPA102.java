4b79c7e3243e3628804ebd31e9767a6577314c0a
I_can't_code <johnson156156156@gmail.com>
2021-04-11 02:39:27 +0800

    上午 02:38 2021/4/11

--- TQC/Java_8/JPA/JPA102.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class JPA102 {
-    public static void main(String[] args) {
-        System.out.print("Please input:");
-        double kg = new Scanner(System.in).nextInt();
-        System.out.printf("%6f kg = %6f ponds",kg,kg*2.20462);
-    }
-}
